13d556b708e8d49c0a7bd78d225726ac164fde52
Jonas Nikula <jonas.nikula@student.tut.fi>
2020-01-18 01:07:46 +0100

    Spawn missiles on a half-circle

--- Missile.gd
+++ Missile.gd
@@ -3,29 +3,40 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var gravity = Vector2(0, 2)
+var acceleration: Vector2
 var velocity: Vector2
 var target: Vector2
-var speed: float
+var max_speed: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 	
-func init(spawn_point_x: float, target: Vector2, speed: float):
-	var translation_vector = Vector2(spawn_point_x, 0)
-	self.translate(translation_vector)
+func init(spawn_point: Vector2, target: Vector2, max_speed: float):
+	print("Spawn point: ", spawn_point)
+	self.translate(spawn_point)
 	self.target = target
-	self.speed = speed
+	self.max_speed = max_speed
 	var pos = self.get_position()
 	var v_diff = target - pos
-	velocity = v_diff.normalized()*speed
+	velocity = v_diff.normalized()*(max_speed/2)
+	acceleration = velocity/10
 	self.look_at(target)
+
+func limit_velocity(vel, max_length):
+	if vel.length() > max_length:
+		return vel.normalized()*max_length
+	else:
+		return vel
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var distance_to_target = self.get_position().distance_to(target) 
-	if distance_to_target <= delta*speed:
+	velocity += gravity + acceleration
+	velocity = limit_velocity(velocity, max_speed)
+	var speed = velocity.length()
+	if self.position.y > 400:
 		self.queue_free()
 	
 	self.translate(delta*velocity)
-	
+	self.look_at(position+velocity)	
--- MissileSpawner.gd
+++ MissileSpawner.gd
@@ -3,10 +3,21 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var spawn_per_minute = 12
+var spawn_per_minute = 120
 var time_between_spawns_s = 60/spawn_per_minute
 var time_since_last_spawn = time_between_spawns_s
-var missile_speed = 20
+var missile_speed = 100
+
+var spawn_half_circle_r = 1000
+var spawn_half_circle_center = Vector2(500, 0)
+var spawn_pt_x_range = [500-spawn_half_circle_r, 500+spawn_half_circle_r]
+
+func half_circle_y(x):
+	# Invariant: x must be in [c-r, c+r]
+	var r = spawn_half_circle_r
+	var c = spawn_half_circle_center.x
+	var y = sqrt(pow(r,2)-pow(x-c, 2))
+	return -y # minus because 2d coordinates: up is down
 
 var rng = RandomNumberGenerator.new()
 var missile = preload("res://Missile.tscn") # Will load when parsing the script.
@@ -24,8 +35,11 @@ func _process(delta):
 		time_since_last_spawn = 0
 
 func spawn_missile():
+	var spawn_y_offset = 300
 	var node = missile.instance()
-	var target = Vector2(500, 400)
-	var spawn_point_x = rng.randf_range(100, 900)
-	node.init(spawn_point_x, target, missile_speed)
+	var target = spawn_half_circle_center
+	target.y += spawn_y_offset
+	var x = rng.randf_range(spawn_pt_x_range[0], spawn_pt_x_range[1])
+	var spawn_point = Vector2(x, half_circle_y(x)+spawn_y_offset)
+	node.init(spawn_point, target, missile_speed)
 	add_child(node)
