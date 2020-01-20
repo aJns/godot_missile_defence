6b941b75716757c0dd56c4c220365917428d2470
Jonas Nikula <jonas.nikula@student.tut.fi>
2020-01-21 00:06:57 +0100

    Start working on projectile class hierarchy

--- MissileSpawner.gd
+++ MissileSpawner.gd
@@ -7,6 +7,7 @@ var spawn_per_minute = 10
 var time_between_spawns_s = 60/spawn_per_minute
 var time_since_last_spawn = time_between_spawns_s
 var missile_speed = 100
+var rocket_thrust = 750
 
 var spawn_half_circle_r = 1000
 var spawn_half_circle_center = Vector2(500, 0)
@@ -20,7 +21,7 @@ func half_circle_y(x):
 	return -y # minus because 2d coordinates: up is down
 
 var rng = RandomNumberGenerator.new()
-var missile = preload("res://Missile.tscn") # Will load when parsing the script.
+var rocket = preload("res://Rocket.tscn") # Will load when parsing the script.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -35,11 +36,14 @@ func _process(delta):
 		time_since_last_spawn = 0
 
 func spawn_missile():
-	var spawn_y_offset = 300
-	var node = missile.instance()
+	var spawn_y_offset = -200
+	var node = rocket.instance()
 	var target = spawn_half_circle_center
 	target.y += spawn_y_offset
 	var x = rng.randf_range(spawn_pt_x_range[0], spawn_pt_x_range[1])
 	var spawn_point = Vector2(x, half_circle_y(x)+spawn_y_offset)
-	node.init(spawn_point, target, missile_speed, Vector2(0,0))
+	node.position = spawn_point
+	var start_v = spawn_point.direction_to(target).normalized() * (missile_speed)
+	node.init(start_v, rocket_thrust, missile_speed)
 	add_child(node)
+	print("Missile spawn at: ", spawn_point)
--- Projectile.gd
+++ Projectile.gd
@@ -1,43 +1,28 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var gravity = Vector2(0, 2)
-var acceleration: Vector2
-var velocity: Vector2
-var has_target = false
-var target: Vector2
-var max_speed: float
 
-var missile_sprite: Node2D
+var grav_a = Vector2(0, 2)
+var velocity: Vector2
+var engine_thrust: float
+var max_speed: float
 
 const MAX_DST_ORIG = 10000
 const GROUND_LEVEL = 500
 
 var explosion = preload("res://Explosion.tscn")
 
+class_name Projectile
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 
-# TODO: Do Missile init better. Plan:
-#	Some parameters as in some structure
-#		Starting: Position, velocity,
-#		max_speed, acceleration
-#	A target 
-	
-func init(spawn_point: Vector2, target_direction: Vector2, max_speed: float, target: Vector2):
-	missile_sprite = get_node("MissileSprite")
-	print("Spawn point: ", spawn_point)
-	self.translate(spawn_point)
+func init(start_velocity: Vector2, engine_thrust: float, max_speed: float):
+	self.velocity = start_velocity
+	self.engine_thrust = engine_thrust
 	self.max_speed = max_speed
-	var pos = self.get_position()
-	var v_diff = target_direction - pos
-	velocity = v_diff.normalized()*(max_speed/2)
-	acceleration = velocity/10
-	self.target = target
+	
 
 func limit_velocity(vel, max_length):
 	if vel.length() > max_length:
@@ -45,11 +30,13 @@ func limit_velocity(vel, max_length):
 	else:
 		return vel
 		
+		
 func explode():
 	var exp_node = explosion.instance()
 	exp_node.position = self.position
 	get_node("/root").add_child(exp_node)
 	self.queue_free()
+
 
 func check_state(delta):
 	if self.position.length() > MAX_DST_ORIG:
@@ -57,14 +44,10 @@ func check_state(delta):
 
 	if self.position.y > GROUND_LEVEL:
 		explode()
-	# TODO: explode if colliding with city, explosion, or ground
-	if has_target:
-		if self.position.distance_to(target) < min(1, velocity.length()*delta):
-			explode()
 
 
 func move(delta):
-	velocity += gravity + acceleration
+	velocity += grav_a + (engine_thrust*velocity.normalized())
 	velocity = limit_velocity(velocity, max_speed)
 	var speed = velocity.length()
 	
