a3f8e2e993c713ce9177820d07b98b0c0c88e19c
Jonas Nikula <jonas.nikula@student.tut.fi>
2020-02-05 22:11:30 +0100

    Tweak chemtrails

--- Chemtrail.gd
+++ Chemtrail.gd
@@ -9,7 +9,8 @@ var current_end_pos: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	width = 2
+	default_color = Color(0.54, 0.66, 0.69, 1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
