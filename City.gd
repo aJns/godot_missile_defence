7f3c6ae74a0d3ea398f911ea28836e8b4dfe4ace
Jonas Nikula <jonas.nikula@gmail.com>
2020-02-12 23:15:59 +0100

    Add city hp label

--- City.gd
+++ City.gd
@@ -6,10 +6,15 @@ extends Node2D
 
 var city_life = 100
 var sprite_node: Sprite
+var label_node: Label
+
+const CITY_HP_STR = "City HP: %s"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	sprite_node = get_node("city_sprite")
+	label_node = get_node("health_label")
+	label_node.text = CITY_HP_STR % city_life
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,4 +26,4 @@ func take_damage(explosion_position: Vector2, explosion_radius: float):
 	var exp_pos_local = explosion_position - self.position
 	if sprite_node.get_rect().has_point(exp_pos_local):
 		city_life -= 1
-		print("City life: ", city_life)
+		label_node.text = CITY_HP_STR % city_life
