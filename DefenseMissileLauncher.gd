7b7d79f47ee1742614165f2fbc1f2b0d9ccdbdc1
Jonas Nikula <jonas.nikula@student.tut.fi>
2020-02-17 19:16:53 +0100

    Use inputmap

--- DefenseMissileLauncher.gd
+++ DefenseMissileLauncher.gd
@@ -5,7 +5,7 @@ extends Node2D
 # var b = "text"
 
 const missile_speed = 100
-var missile_scene = preload("res://Projectile.tscn") 
+var missile_scene = preload("res://Projectile.tscn")
 
 
 enum MSL_COLOR{
@@ -18,17 +18,20 @@ func _ready():
 	pass # Replace with function body.
 
 
-# TODO: this doesn't work with touch input
+func _input(event):
+	if event.is_action_pressed("game_missile_shoot"):
+		shoot_missile_at(event.position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("game_missile_shoot"):
-		var target = get_viewport().get_mouse_position()
-		var msl = missile_scene.instance()
-		
-		msl.position = self.position
-		var flight_dir = msl.position.direction_to(target)
-		get_node("/root").add_child(msl) # For some reason if add as child to missile_defense to missile is invisible
-		# TODO: find out why ^^
-		msl.init(flight_dir, 100, target, true)
-		msl.set_sprite_color(MSL_COLOR.BLUE)
+	pass
+	
+func shoot_missile_at(target: Vector2):
+	var msl = missile_scene.instance()
+	
+	msl.position = self.position
+	var flight_dir = msl.position.direction_to(target)
+	get_node("/root").add_child(msl) # For some reason if add as child to missile_defense to missile is invisible
+	# TODO: find out why ^^
+	msl.init(flight_dir, 100, target, true)
+	msl.set_sprite_color(MSL_COLOR.BLUE)
