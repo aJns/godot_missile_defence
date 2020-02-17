4af9f7ca60412efbc4c53a1b11dde1315e201e41
Jonas Nikula <jonas.nikula@student.tut.fi>
2020-02-17 17:23:21 +0100

    Add menu

--- Menu.gd
+++ Menu.gd
@@ -1,16 +1,21 @@
-extends Node2D
+extends Control
+
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func game_over():
-	get_node("GameOverLabel").visible = true
+
+func _on_new_game_pressed():
+	print("Start new game")
+	self.visible = !self.visible
